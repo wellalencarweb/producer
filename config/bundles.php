776bb2888a8cf1ca828df79675f5f136b9b48025
wellington_alencar <wellington.alencar@pravaler.com.br>
2020-07-31 19:00:24 -0300

    instalando dependências

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     OldSound\RabbitMqBundle\OldSoundRabbitMqBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
